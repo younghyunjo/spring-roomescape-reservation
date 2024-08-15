83d3f0eab79a742b5eede146f6185803a2e4d598
younghyun <younghyunjo99@gmail.com>
2024-08-15 15:50:29 +0900

    test:테스트 환경 만듬

---
+++ src/test/java/roomescape/reservation/ReservationServiceTest.kt
@@ -0,0 +1,17 @@
+package roomescape.reservation
+
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+
+class ReservationServiceTest :
+    BehaviorSpec(
+        {
+            given("test") {
+                `when`("test") {
+                    then("test") {
+                        1 shouldBe 1
+                    }
+                }
+            }
+        },
+    )
