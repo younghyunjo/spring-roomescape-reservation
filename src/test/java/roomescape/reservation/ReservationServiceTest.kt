834d799e226810b1be84bcd4edbdd1f811148316
younghyun <younghyunjo99@gmail.com>
2024-08-15 18:03:38 +0900

    test:MockK 설정

--- src/test/java/roomescape/reservation/ReservationServiceTest.kt
+++ src/test/java/roomescape/reservation/ReservationServiceTest.kt
@@ -1,17 +1,26 @@
 package roomescape.reservation
 
+import com.ninjasquad.springmockk.MockkBean
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
+import io.mockk.every
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
 
-class ReservationServiceTest :
-    BehaviorSpec(
-        {
-            given("test") {
-                `when`("test") {
-                    then("test") {
-                        1 shouldBe 1
-                    }
+@SpringBootTest
+class ReservationServiceTest(
+    @MockkBean val reservationRepository: ReservationRepository,
+    @Autowired val reservationService: ReservationService,
+) : BehaviorSpec({
+        Given("reservation") {
+            val r = Reservation(name = "name", date = "date", time = "time")
+            When("add") {
+                every { reservationRepository.save(r) } returns r.copy(id = 1)
+
+                val newReservation = reservationService.add(r)
+                Then("id is setted") {
+                    newReservation.id shouldBe 1
                 }
             }
-        },
-    )
+        }
+    })
